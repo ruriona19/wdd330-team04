89ce47a953747a1028cff7844d9dbe9a1292fa31
Roberto Uriona <roberto.uriona@jalasoft.com>
2023-09-23 10:11:40 -0400

    Adding isCartEmpty function

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,25 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+import { getLocalStorage } from "./utils.mjs";
 
-let products = [];
+/**
+ * Returns true if the local storage object that contains the
+ * data related to the Cart is empty
+ * @param {String} localStorageKey The name of the localStorage key.
+ * @return {Boolean} A boolean flag to determine wheter the cart is empty or not.
+ */
+function isCartEmpty(localStorageKey) {
+  return (getLocalStorage(localStorageKey) === null) ? true : false;
+}
 
 function addProductToCart(product) {
+  let products;
+  if(isCartEmpty("so-cart")){
+    products = [];
+  }else{
+    products = getLocalStorage("so-cart");
+  }
+
   products.push(product);
   setLocalStorage("so-cart", products);
 }
