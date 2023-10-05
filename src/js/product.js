aa6b23cd220ce0723c791881ed55b5c817e08177
galiciaabraham <galicia.abraham@hotmail.com>
2023-10-05 12:57:06 -0500

    changed product_pages/index I removed the reference to main from the index page in product_pages, since the only relevant section of code being used is the one for the adding animation and not the list of products happening on main, I think it's best if we don't call main into the product pages, to avoid a runtime error.

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,9 @@
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
+import { getCartCountFromLocalStorage } from "./utils.mjs";
+
+const backpackBadge = document.getElementById("cart-count");
+backpackBadge.innerHTML = getCartCountFromLocalStorage();
 
 const productId = getParam("product");
 productDetails(productId);
